667f28e4e0196fdf25444cdf4fc6821b90747396
DDC.Mac2 <ddc.mac2@ddc-mac2-mac.local>
2017-10-05 09:49:02 -0500

    Added automation tests for interacting with tab bar items

--- ToolBarExample/KYTCustomAlerts.m
+++ ToolBarExample/KYTCustomAlerts.m
@@ -14,28 +14,28 @@
 {
     NSValue *toastCenterPoint = [NSValue valueWithCGPoint:CGPointMake([[UIScreen mainScreen] bounds].size.width/2.0, [[UIScreen mainScreen] bounds].size.height/3.0)];
     
-    [view makeToast:@"Creating object to add to Persistence Layer" duration:1.0 position:toastCenterPoint];
+    [view makeToast:@"Creating object to add to Persistence Layer" duration:5.0 position:toastCenterPoint];
 }
 
 + (void)alertUserWithDeleteMessage:(UIView *)view
 {
     NSValue *toastCenterPoint = [NSValue valueWithCGPoint:CGPointMake([[UIScreen mainScreen] bounds].size.width/2.0, [[UIScreen mainScreen] bounds].size.height/3.0)];
     
-    [view makeToast:@"Deleteing object from Persistence Layer" duration:1.0 position:toastCenterPoint];
+    [view makeToast:@"Deleteing object from Persistence Layer" duration:5.0 position:toastCenterPoint];
 }
 
 + (void)alertUserWithUpdateMessage:(UIView *)view
 {
     NSValue *toastCenterPoint = [NSValue valueWithCGPoint:CGPointMake([[UIScreen mainScreen] bounds].size.width/2.0, [[UIScreen mainScreen] bounds].size.height/3.0)];
     
-    [view makeToast:@"Updating object to Persistence Layer" duration:1.0 position:toastCenterPoint];
+    [view makeToast:@"Updating object to Persistence Layer" duration:5.0 position:toastCenterPoint];
 }
 
 + (void)alertUserWithReadMessage:(UIView *)view
 {
     NSValue *toastCenterPoint = [NSValue valueWithCGPoint:CGPointMake([[UIScreen mainScreen] bounds].size.width/2.0, [[UIScreen mainScreen] bounds].size.height/3.0)];
     
-    [view makeToast:@"Reading object from Persistence Layer" duration:1.0 position:toastCenterPoint];
+    [view makeToast:@"Reading object from Persistence Layer" duration:5.0 position:toastCenterPoint];
 }
 
 @end
